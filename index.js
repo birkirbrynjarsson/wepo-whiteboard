7eb2eabbfb1081176ea11fd3d0e46a42272d26ed
Birkir Brynjarsson <birkir@birkir.is>
2017-01-17 15:29:12 +0000

    Added package.json and gitignore file

---
+++ index.js
@@ -0,0 +1,29 @@
+class Shape {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    doStuff() {
+        //
+    }
+}
+
+class Rectangle extends Shape {
+    constructor() {
+        super();
+    }
+    doStuff() {
+        super.doStuff();
+    }
+}
+
+var canvasObj = document.getElementById("myCanvas");
+
+$("myCanvas").on("mousedown", function () {
+    var shape = undefined;
+    if (nextObject === "Circle") {
+        // TODO: create an instance of circle
+    } else if (nextObject === "Rectangle") {
+
+    }
+});
