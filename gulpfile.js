cfb87953408718488dadbc5a3b6624763e14cce3
HaraldurShoshan <haraldurshoshan@gmail.com>
2017-01-17 16:43:42 +0000

    added browsersync, css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,33 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('task-name', function() {
-    // Stuff here
-})
+gulp.task('browserSync', function() {
+    browserSync.init(null, {
+        proxy: "http://localhost:3000",
+        files: ["**/*.*"],
+        port: 8000
+    });
+});
+
+gulp.task("css", function(){
+    return gulp.src("app/css/**/*.css")
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task("html", function(){
+    return gulp.src("app/**/*.html")
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task("watch", function(){
+    gulp.watch("app/css/**/*.css", ["css"]);
+    gulp.watch("app/**/*.html", ["html"]);
+});
+
+gulp.task("default", ["browserSync", "css", "watch"], function() {
+    
+});
