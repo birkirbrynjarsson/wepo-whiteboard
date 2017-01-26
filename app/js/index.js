afa564a44a56036d7499b5e28ad8bd9ee36644b9
HaraldurShoshan <haraldurshoshan@gmail.com>
2017-01-26 18:46:58 +0000

    ui work

--- app/js/index.js
+++ app/js/index.js
@@ -12,6 +12,25 @@ window.onresize = function() {
 }
 
 $(document).ready(function(){
+
+  //thickness range Slider 
+  var rangeSlider = function(){
+      var slider = $('.range-slider'),
+      range = $('.range-slider__range'),
+      value = $('.range-slider__value');
+      slider.each(function(){
+          value.each(function(){
+              var value = $(this).prev().attr('value');
+              $(this).html(value);
+          });
+          range.on('input', function(){
+              $(this).next(value).html(this.value);
+          });
+      });
+   };
+
+    rangeSlider();
+
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
 
