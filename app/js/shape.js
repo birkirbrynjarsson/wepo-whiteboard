1f36f29bee81537634f992dd072d93191d32fc78
Orri Axels <orriaxels@gmail.com>
2017-01-27 10:52:29 +0000

    shape classes

--- app/js/shape.js
+++ app/js/shape.js
@@ -1,10 +1,10 @@
 class Shape {
-    constructor(startX, startY, endX, endY, color, width) {
+    constructor(startX, startY, endX, endY, col, width) {
         this.startX = startX;
         this.startY = startY;
         this.endX = endX;
         this.endY = endY;
-        this.color = color;
+        this.col = col;
         this.width = width;
     }
 /*    doStuff() {
@@ -12,19 +12,57 @@ class Shape {
     }*/
 }
 
+class Pen extends Shape {
+    constructor(startX, startY, color) {
+        super(startX, startY, color);
+    }
+
+    doStuff() {
+        super.doStuff();
+    }
+
+    draw(context) {
+
+    }       
+}
+
 class Rectangle extends Shape {
-    constructor() {
-        super();
+    constructor(startX, startY, col, drawing) {
+        super(startX, startY, col, drawing);
+    }
+    
+    doStuff() {
+        super.doStuff();
+    }
+
+    draw(context) {
+        context.fillStyle = '#'+this.col;
+        context.strokeRect(this.startX, this.startY, 50, 50);
+        //console.log('x: ' + this.startX, 'y: ' + this.startY);
+        console.log(this.col);
     }
 }
 
 class Circle extends Shape {
-    constructor() {
-        super();
+    constructor(startX, startY, color) {
+        super(startX, startY, color);
     }
-/*    doStuff() {
+
+    doStuff() {
         super.doStuff();
-    }*/
+    }
+
+    draw(context) {
+        context.beginPath( );
+        context.arc( this.startX, this.startY, 70, 0, 2 * Math.PI, false );
+        // check out the parameters here
+        context.fillStyle = "#"+this.color;
+        context.fill( );
+        context.lineWidth = 5;
+        context.strokeStyle = "black";
+        context.stroke( );
+        console.log(this.color);
+    }
 }
 
 class Text extends Shape {
